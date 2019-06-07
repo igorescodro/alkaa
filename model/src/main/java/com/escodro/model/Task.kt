7d9287a1dc4f7f395ab4f1228cecefabba256402
Igor Escodro <escodro@outlook.com>
2019-06-07 14:46:03 -0300

    :building_construction: Create Model module
    Module created with model

---
+++ model/src/main/java/com/escodro/model/Task.kt
@@ -0,0 +1,30 @@
+package com.escodro.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.util.Calendar
+
+/**
+ * [Entity] to represent a task.
+ */
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Category::class,
+        parentColumns = ["category_id"],
+        childColumns = ["task_category_id"],
+        onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index(value = ["task_category_id"])]
+
+)
+data class Task(
+    @ColumnInfo(name = "task_id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "task_is_completed") var completed: Boolean = false,
+    @ColumnInfo(name = "task_title") var title: String,
+    @ColumnInfo(name = "task_description") var description: String? = null,
+    @ColumnInfo(name = "task_category_id") var categoryId: Long? = null,
+    @ColumnInfo(name = "task_due_date") var dueDate: Calendar? = null
+)

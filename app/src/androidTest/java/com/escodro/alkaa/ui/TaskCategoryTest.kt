ac95c05f235f44cdbf064d77c89387cda68a2380
Igor Escodro <escodro@outlook.com>
2019-06-21 22:26:44 -0300

    :white_check_mark: Add test to validate fix
    Test added to validate category removal fix

---
+++ app/src/androidTest/java/com/escodro/alkaa/ui/TaskCategoryTest.kt
@@ -0,0 +1,69 @@
+package com.escodro.alkaa.ui
+
+import com.escodro.alkaa.R
+import com.escodro.alkaa.framework.AcceptanceTest
+import com.escodro.alkaa.ui.main.MainActivity
+import com.escodro.model.Category
+import org.junit.After
+import org.junit.Test
+
+/**
+ * Test class to validate the Task and Category screens flow.
+ */
+class TaskCategoryTest : AcceptanceTest<MainActivity>(MainActivity::class.java) {
+
+    @After
+    fun cleanTable() {
+        daoProvider.getTaskDao().cleanTable().blockingGet()
+        daoProvider.getCategoryDao().cleanTable().blockingGet()
+    }
+
+    @Test
+    fun addTaskAfterRemovingCategoryAndReturningToScreen() {
+        val categoryName = "Temporary"
+        val taskName = "Should be deleted together"
+        val category = Category(name = categoryName, color = "#dd55ff")
+        daoProvider.getCategoryDao().insertCategory(category).blockingAwait()
+        navigateToCategory(categoryName)
+        addTask(taskName)
+        navigateToCategoryScreen()
+        removeCategory(categoryName)
+        events.navigateUp()
+        checkThat.listNotContainsItem(R.id.recyclerview_tasklist_list, taskName)
+        checkThat.viewHasText(R.id.toolbar_title, R.string.task_list_default_title)
+        addTask("This is a new task in a new list")
+    }
+
+    private fun addTask(taskName: String) {
+        events.clickOnView(R.id.edittext_itemadd_description)
+        events.textOnView(R.id.edittext_itemadd_description, taskName)
+        events.pressImeActionButton(R.id.edittext_itemadd_description)
+        events.waitFor(1000)
+        checkThat.listContainsItem(R.id.recyclerview_tasklist_list, taskName)
+    }
+
+    private fun removeCategory(categoryName: String) {
+        events.clickOnView(R.id.imageview_itemcategory_options)
+        events.clickOnViewWithText(R.string.category_list_menu_remove)
+        events.clickOnViewWithText(R.string.category_list_dialog_remove_positive)
+        checkThat.listNotContainsItem(R.id.recyclerview_categorylist_list, categoryName)
+        checkThat.viewIsCompletelyDisplayed(R.id.textview_categorylist_empty)
+    }
+
+    private fun openDrawer() {
+        events.openDrawer(R.id.drawer_layout_main_parent)
+        checkThat.drawerIsOpen(R.id.drawer_layout_main_parent)
+    }
+
+    private fun navigateToCategoryScreen() {
+        openDrawer()
+        events.clickOnViewWithText(R.string.drawer_menu_manage_categories)
+        checkThat.viewHasText(R.id.toolbar_title, R.string.category_list_label)
+    }
+
+    private fun navigateToCategory(categoryName: String) {
+        openDrawer()
+        events.clickOnViewWithText(categoryName)
+        checkThat.viewHasText(R.id.toolbar_title, categoryName)
+    }
+}

da242adf967ab2a4c781e68e31f5f5af0e97ff34
Igor Escodro <escodro@outlook.com>
2019-11-25 18:53:08 -0300

    :sparkles: Create base Repository interface implementation
    Data source interfaces created to abstract the access to data (local,
    remote etc)

---
+++ data/repository/src/main/java/com/escodro/repository/model/Category.kt
@@ -0,0 +1,10 @@
+package com.escodro.repository.model
+
+/**
+ * Data class to represent a Category.
+ */
+data class Category(
+    val id: Long = 0,
+    val name: String?,
+    val color: String?
+)

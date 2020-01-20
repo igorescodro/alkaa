8b0abe0f0596cc2252985440e9678f4a41419f48
Igor Escodro <escodro@outlook.com>
2020-01-20 14:47:54 -0300

    :card_file_box: Create migration
    Migration from 2 to 3 created

--- data/local/src/main/java/com/escodro/local/migration/Migration.kt
+++ data/local/src/main/java/com/escodro/local/migration/Migration.kt
@@ -12,3 +12,14 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL("ALTER TABLE Task ADD COLUMN task_completed_date INTEGER")
     }
 }
+
+/**
+ * SQLite Database migration from V2 to V3. This migration includes new fields on Task table.
+ */
+@Suppress("MagicNumber")
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE Task ADD COLUMN task_is_repeating INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE Task ADD COLUMN task_alarm_interval INTEGER")
+    }
+}

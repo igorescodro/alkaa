f61685eb497bd0ccfc546f049c814b9781c30640
Igor Escodro <escodro@outlook.com>
2020-03-02 12:46:03 -0300

    :lipstick: Create base Search fragment interaction
    Simple animation created using Motion Layout

--- features/search/src/main/java/com/escodro/search/SearchFragment.kt
+++ features/search/src/main/java/com/escodro/search/SearchFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_search.*
 
 /**
  * Fragment to search for Tasks.
@@ -15,6 +16,17 @@ internal class SearchFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_search, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_search, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        layout_search.transitionToEnd()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        layout_search.transitionToStart()
+    }
 }

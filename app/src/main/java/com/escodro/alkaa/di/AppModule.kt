6ca3254864020a2c4342003a7e9e27148462fc30
Igor Escodro <escodro@outlook.com>
2020-09-17 08:55:29 -0300

    :fire: Remove current Activity code
    Let's try Jetpack Compose! But before we get started, let's remove the
    current Activity once it the Kotlin Accessor conflicts with Compose.
    All the current presentation code was removed to open space to new ones!
    That's exciting!

--- app/src/main/java/com/escodro/alkaa/di/AppModule.kt
+++ app/src/main/java/com/escodro/alkaa/di/AppModule.kt
@@ -1,15 +1,11 @@
 package com.escodro.alkaa.di
 
 import com.escodro.alkaa.mapper.CategoryMapper
-import com.escodro.alkaa.presentation.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
  * Application module.
  */
 val appModule = module {
-    viewModel { MainViewModel(get(), get()) }
-
     factory { CategoryMapper() }
 }

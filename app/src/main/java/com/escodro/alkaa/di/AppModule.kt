adca76c2bbf10ba1ed8997b53375ae4fa36a9ccd
Igor Escodro <escodro@outlook.com>
2020-09-18 11:14:53 -0300

    :fire: Remove more unused code
    Now that we have some stable code running, time for more cleanup!

--- app/src/main/java/com/escodro/alkaa/di/AppModule.kt
+++ app/src/main/java/com/escodro/alkaa/di/AppModule.kt
@@ -1,11 +1,8 @@
 package com.escodro.alkaa.di
 
-import com.escodro.alkaa.mapper.CategoryMapper
 import org.koin.dsl.module
 
 /**
  * Application module.
  */
-val appModule = module {
-    factory { CategoryMapper() }
-}
+val appModule = module {}

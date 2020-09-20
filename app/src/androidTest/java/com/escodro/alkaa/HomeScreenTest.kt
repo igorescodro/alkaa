5d5cf0206d03ed63d6692aacc9e358086b872d93
Igor Escodro <escodro@outlook.com>
2020-09-20 17:00:14 -0300

    :white_check_mark: Create a new UI test
    New test created to validate if the top bar title matches the bottom
    bar selected icon.
    In order to make the test possible (and help users), accessibility
    description was added in the icons.

---
+++ app/src/androidTest/java/com/escodro/alkaa/HomeScreenTest.kt
@@ -0,0 +1,38 @@
+package com.escodro.alkaa
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.ui.test.createComposeRule
+import androidx.ui.test.onNodeWithLabel
+import androidx.ui.test.onNodeWithText
+import androidx.ui.test.performClick
+import com.escodro.alkaa.model.HomeSection
+import com.escodro.alkaa.presentation.home.Home
+import com.escodro.theme.AlkaaTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+internal class HomeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            AlkaaTheme {
+                Home()
+            }
+        }
+    }
+
+    @Test
+    fun test_assertIfTitleChangesWhenBottomIconIsSelected() {
+        HomeSection.values().forEach { section ->
+            onNodeWithLabel(context.getString(section.title)).performClick()
+            onNodeWithText(context.getString(section.title)).assertExists()
+        }
+    }
+}

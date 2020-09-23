eca74721d1dc2a2b0e427db095a5deeb076cc912
Igor Escodro <escodro@outlook.com>
2020-09-23 10:14:21 -0300

    :fire: Remove all Task UI code
    All the task UI related were removed to be replaced with Jetpack Compose

--- features/task/src/main/java/com/escodro/task/di/TaskModule.kt
+++ features/task/src/main/java/com/escodro/task/di/TaskModule.kt
@@ -4,26 +4,12 @@ import com.escodro.task.mapper.AlarmIntervalMapper
 import com.escodro.task.mapper.CategoryMapper
 import com.escodro.task.mapper.TaskMapper
 import com.escodro.task.mapper.TaskWithCategoryMapper
-import com.escodro.task.presentation.detail.TaskDetailProvider
-import com.escodro.task.presentation.detail.TaskDetailProviderImpl
-import com.escodro.task.presentation.detail.alarm.TaskAlarmViewModel
-import com.escodro.task.presentation.detail.category.TaskCategoryViewModel
-import com.escodro.task.presentation.detail.main.TaskDetailViewModel
-import com.escodro.task.presentation.list.TaskListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
  * Task dependency injection module.
  */
 val taskModule = module {
-
-    single<TaskDetailProvider> { TaskDetailProviderImpl(get(), get()) }
-
-    viewModel { TaskListViewModel(get(), get(), get(), get(), get(), get(), get(), get()) }
-    viewModel { TaskDetailViewModel(get(), get(), get(), get()) }
-    viewModel { TaskCategoryViewModel(get(), get(), get(), get(), get()) }
-    viewModel { TaskAlarmViewModel(get(), get(), get(), get(), get()) }
 
     // Mappers
     factory { AlarmIntervalMapper() }

38ec23778857c3143cea7e4d65c4f94549c27763
Igor Escodro <escodro@outlook.com>
2020-11-24 09:23:32 -0300

    :sparkles: Implement the Task Section screen
    New screen created with a simple Task Section and the items with the
    same UI as the Alkaa 1.0.

--- libraries/theme/src/main/java/com/escodro/theme/Type.kt
+++ libraries/theme/src/main/java/com/escodro/theme/Type.kt
@@ -2,7 +2,6 @@ package com.escodro.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -11,13 +10,15 @@ import androidx.compose.ui.unit.sp
  */
 val typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     h5 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Thin,
         fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
     )
 )

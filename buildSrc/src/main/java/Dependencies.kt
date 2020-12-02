1c2a8b76c7d2fc9c16f86e52c8f87113df67308d
Igor Escodro <escodro@outlook.com>
2020-12-02 08:23:24 -0300

    :arrow_up: Upgrade Gradle version
    Updated the IDE to latest Android Studio Arctic Fox and Gradle Plugin
    to 7.0.0-alpha01

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -44,7 +44,7 @@ object Versions {
 
     const val compose = "1.0.0-alpha07"
 
-    const val buildGradle = "4.2.0-alpha16"
+    const val buildGradle = "7.0.0-alpha01"
 
     const val detekt = "1.13.1"
     const val ktlint = "0.39.0"

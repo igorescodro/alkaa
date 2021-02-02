b3f7a1305f8f83fc31d4a73a800090bdbd0a7aa1
Igor Escodro <escodro@outlook.com>
2021-02-02 17:00:38 -0300

    :white_check_mark: Update and create tests for TaskListViewModel
    Update the shameful test that was created and updated with better ones.
    The next commit will introduce better state handle so the tests will
    be updated as well.

--- features/task/src/test/java/com/escodro/task/presentation/list/TaskListViewModelTest.kt
+++ features/task/src/test/java/com/escodro/task/presentation/list/TaskListViewModelTest.kt
@@ -6,9 +6,15 @@ import com.escodro.task.mapper.TaskMapper
 import com.escodro.task.mapper.TaskWithCategoryMapper
 import com.escodro.task.presentation.list.fake.LoadUncompletedTasksFake
 import com.escodro.task.presentation.list.fake.UpdateTaskStatusFake
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 internal class TaskListViewModelTest {
 
     private val loadUncompletedTasks = LoadUncompletedTasksFake()
@@ -27,7 +33,58 @@ internal class TaskListViewModelTest {
     }
 
     @Test
-    fun `basic test`() {
+    fun `test if when there are uncompleted items, they are returned`() = runBlockingTest {
+
+        // Given the use case returns the list with uncompleted tasks
+        loadUncompletedTasks.returnDefaultValues()
         viewModel.loadTasks()
+
+        // When the latest event is collected
+        val result = arrayListOf<TaskListViewState>()
+        val job = launch { viewModel.state.toList(result) }
+
+        // Then that state contains the list with uncompleted tasks
+        Assert.assertTrue(result.first().items.isNotEmpty())
+
+        job.cancel()
+    }
+
+    @Test
+    fun `test if when there are no uncompleted items, a empty list is returned`() =
+        runBlockingTest {
+
+            // Given the use case returns an empty list
+            loadUncompletedTasks.clean()
+            viewModel.loadTasks()
+
+            // When the latest event is collected
+            val result = arrayListOf<TaskListViewState>()
+            val job = launch { viewModel.state.toList(result) }
+
+            // Then that state contains the empty list
+            Assert.assertTrue(result.first().items.isEmpty())
+
+            job.cancel()
+        }
+
+    @Test
+    fun `test if state is re-triggered when list changes`() = runBlockingTest {
+
+        // Given collecting multiple states
+        val result = arrayListOf<TaskListViewState>()
+        val job = launch { viewModel.state.toList(result) }
+
+        // When multiple states are sent
+        loadUncompletedTasks.clean()
+        viewModel.loadTasks()
+        loadUncompletedTasks.returnDefaultValues()
+        viewModel.loadTasks()
+
+        // Then multiple states are collected
+        Assert.assertTrue(result.size == 2)
+        Assert.assertTrue(result[0].items.isEmpty())
+        Assert.assertTrue(result[1].items.isNotEmpty())
+
+        job.cancel()
     }
 }

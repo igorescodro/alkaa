172ef5dde72eda7ee2bf7bd032b50e527b47d74f
Igor Escodro <escodro@outlook.com>
2021-02-08 15:40:48 -0300

    :fire: Remove all tracker-related code
    Let's clean up the project and removing this code will make it easier
    to clean dependencies.

--- features/tracker/src/main/java/com/escodro/tracker/di/TrackerModule.kt
+++ features/tracker/src/main/java/com/escodro/tracker/di/TrackerModule.kt
@@ -1,8 +1,5 @@
 package com.escodro.tracker.di
 
-import com.escodro.tracker.mapper.TrackerMapper
-import com.escodro.tracker.presentation.TrackerViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
@@ -19,8 +16,4 @@ private val loadFeatureModules by lazy {
 /**
  * Tracker dependency injection module.
  */
-val trackerModule = module {
-    viewModel { TrackerViewModel(get(), get()) }
-
-    factory { TrackerMapper() }
-}
+val trackerModule = module { }

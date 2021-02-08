70f5d2f00ad4ad4c5b54982aaa8995ae790f8677
Igor Escodro <escodro@outlook.com>
2021-02-08 15:21:29 -0300

    :fire: Remove all category-related code
    Let's clean up the project and removing this code will make it easier
    to clean dependencies.

--- features/category/src/main/java/com/escodro/category/di/CategoryModule.kt
+++ features/category/src/main/java/com/escodro/category/di/CategoryModule.kt
@@ -1,21 +1,8 @@
 package com.escodro.category.di
 
-import com.escodro.category.mapper.CategoryMapper
-import com.escodro.category.presentation.CategoryDetailViewController
-import com.escodro.category.presentation.add.CategoryAddViewModel
-import com.escodro.category.presentation.edit.CategoryEditViewModel
-import com.escodro.category.presentation.list.CategoryListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
  * Category dependency injection module.
  */
-val categoryModule = module {
-    viewModel { CategoryListViewModel(get(), get(), get()) }
-    viewModel { CategoryEditViewModel(get(), get(), get()) }
-    viewModel { CategoryAddViewModel(get(), get()) }
-
-    factory { CategoryMapper() }
-    factory { CategoryDetailViewController() }
-}
+val categoryModule = module { }

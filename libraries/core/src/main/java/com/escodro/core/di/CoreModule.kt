4021b17086826d8568dadad2181c7ec65ba29091
Igor Escodro <escodro@outlook.com>
2021-02-10 08:15:23 -0300

    :fire: Remove more unused dependencies and extensions
    The cleanup goes on...

--- libraries/core/src/main/java/com/escodro/core/di/CoreModule.kt
+++ libraries/core/src/main/java/com/escodro/core/di/CoreModule.kt
@@ -1,12 +1,9 @@
 package com.escodro.core.di
 
-import com.escodro.core.viewmodel.ToolbarViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
  * Core dependency injection module.
  */
 val coreModule = module {
-    viewModel { ToolbarViewModel() }
 }

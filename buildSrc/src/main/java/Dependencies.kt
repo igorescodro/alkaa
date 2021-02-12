0a05d6461bcfa7f61cb12c6a3836e34aa617bf7e
Igor Escodro <escodro@outlook.com>
2021-02-12 09:29:36 -0300

    ⬆️ Upgrade Compose Navigation dependency to match latest version
    The Compose Navigation must match the Compose version (even if they
    have different version number. That's great, thank you Google) so now
    it is updated ad R8 stopped complaining about duplicate classes.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -31,7 +31,7 @@ object Versions {
     const val testRoom = "2.1.0"
 
     const val compose = "1.0.0-alpha11"
-    const val composeNav = "1.0.0-alpha05"
+    const val composeNav = "1.0.0-alpha06"
 
     const val buildGradle = "7.0.0-alpha06"
 

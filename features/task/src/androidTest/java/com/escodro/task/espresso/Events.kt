b470e38ea4b1a033cd6f1fa3c0e699b4a3b1c70d
Igor Escodro <escodro@outlook.com>
2021-02-24 11:14:39 -0300

    ♻️ Update some tests to be flaky
    Unfortunately when mixing Jetpack Compose with default Android View
    System components, some delay were found during instrumented tests.
    So the new annotation was added to be more permissive with this class.
    Also Barista is now used instead of vanilla Espresso in order to use
    rules and more simple syntax.

--- features/task/src/androidTest/java/com/escodro/task/espresso/Events.kt
+++ features/task/src/androidTest/java/com/escodro/task/espresso/Events.kt
@@ -1,39 +1,12 @@
 package com.escodro.task.espresso
 
-import android.R
-import android.widget.DatePicker
-import android.widget.TimePicker
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.PickerActions
-import androidx.test.espresso.matcher.ViewMatchers
-import org.hamcrest.Matchers
+import com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setDateOnPicker
+import com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setTimeOnPicker
 import java.util.Calendar
 
 internal fun setDateTime(calendar: Calendar) {
-    setDate(calendar)
-    Espresso.onView(ViewMatchers.withId(R.id.button1)).perform(ViewActions.click())
-    setTime(calendar)
-    Espresso.onView(ViewMatchers.withId(R.id.button1)).perform(ViewActions.click())
-}
-
-private fun setDate(calendar: Calendar) {
-    Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(DatePicker::class.java.name)))
-        .perform(
-            PickerActions.setDate(
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH) + 1,
-                calendar.get(Calendar.DAY_OF_MONTH)
-            )
-        )
-}
-
-private fun setTime(calendar: Calendar) {
-    Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker::class.java.name)))
-        .perform(
-            PickerActions.setTime(
-                calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE)
-            )
-        )
+    with(calendar) {
+        setDateOnPicker(get(Calendar.YEAR), get(Calendar.MONTH) + 1, get(Calendar.DAY_OF_MONTH))
+        setTimeOnPicker(get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE))
+    }
 }

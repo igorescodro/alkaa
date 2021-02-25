6870102920810f2ac25c1f4407a5e83a18465f34
Igor Escodro <escodro@outlook.com>
2021-02-25 09:53:36 -0300

    ✨ Implement basic Search screen behavior
    For now the basic structure to observe states and the empty list screen
    were created. Next commit will implement the search list.

--- features/search/src/main/java/com/escodro/search/presentation/Search.kt
+++ features/search/src/main/java/com/escodro/search/presentation/Search.kt
@@ -1,13 +1,29 @@
 package com.escodro.search.presentation
 
+import android.graphics.Color
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.escodro.search.R
+import com.escodro.search.model.TaskSearchItem
 import com.escodro.theme.AlkaaTheme
+import com.escodro.theme.components.DefaultIconTextContent
+import org.koin.androidx.compose.getViewModel
 
 /**
  * Alkaa Search Section.
@@ -20,27 +36,100 @@ fun SearchSection(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun SearchLoader(modifier: Modifier = Modifier) {
-    SearchScaffold(modifier = modifier)
+private fun SearchLoader(
+    modifier: Modifier = Modifier,
+    viewModel: SearchViewModel = getViewModel()
+) {
+    val viewState by viewModel.state
+
+    SearchScaffold(viewState = viewState, modifier = modifier)
 }
 
 @Composable
-internal fun SearchScaffold(modifier: Modifier = Modifier) {
+internal fun SearchScaffold(modifier: Modifier = Modifier, viewState: SearchViewState) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
         backgroundColor = MaterialTheme.colors.background
     ) {
-        Text("This is the search")
+        Column(modifier = Modifier.fillMaxWidth()) {
+            val (query, setQuery) = savedInstanceState { "" }
+
+            SearchTextField(query, setQuery)
+            when (viewState) {
+                SearchViewState.Empty -> SearchEmptyContent()
+                is SearchViewState.Loaded -> SearchListContent()
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchTextField(text: String, onTextChanged: (String) -> Unit) {
+    TextField(
+        value = text,
+        onValueChange = onTextChanged,
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(id = R.string.search_cd_icon)
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    )
+}
+
+@Composable
+private fun SearchEmptyContent() {
+    DefaultIconTextContent(
+        icon = Icons.Outlined.ExitToApp,
+        iconContentDescription = R.string.search_cd_empty_list,
+        header = R.string.search_header_empty
+    )
+}
+
+@Composable
+private fun SearchListContent() {
+    // TODO implement task search list
+}
+
+@Suppress("UndocumentedPublicFunction")
+@Preview
+@Composable
+fun SearchLoadedListPreview() {
+    val task1 = TaskSearchItem(
+        completed = true,
+        title = "Call Me By Your Name",
+        categoryColor = Color.GREEN,
+        isRepeating = false
+    )
+
+    val task2 = TaskSearchItem(
+        completed = false,
+        title = "The Crown",
+        categoryColor = Color.BLUE,
+        isRepeating = true
+    )
+
+    val taskList = listOf(task1, task2)
+
+    AlkaaTheme {
+        SearchScaffold(
+            modifier = Modifier,
+            viewState = SearchViewState.Loaded(taskList)
+        )
     }
 }
 
 @Suppress("UndocumentedPublicFunction")
 @Preview
 @Composable
-fun SearchScaffoldPreview() {
+fun SearchEmptyListPreview() {
     AlkaaTheme {
         SearchScaffold(
-            modifier = Modifier
+            modifier = Modifier,
+            viewState = SearchViewState.Empty
         )
     }
 }

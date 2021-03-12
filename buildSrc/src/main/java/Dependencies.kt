67607ab59b9f5e96747852953b096909e5ee6861
Igor Escodro <escodro@outlook.com>
2021-03-12 08:05:48 -0300

    ⬆️ Upgrade Compose to latest Beta
    Compose updated to Beta-02 and all Compose-related libraries upgraded
    to its respective versions.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val targetSdk = 30
     const val minSdk = 23
 
-    const val kotlin = "1.4.30"
+    const val kotlin = "1.4.31"
     const val material = "1.3.0"
     const val constraintLayout = "2.0.4"
     const val ktx = "1.0.2"
@@ -33,12 +33,12 @@ object Versions {
 
     const val espresso = "3.3.0"
 
-    const val compose = "1.0.0-beta01"
-    const val composeNav = "1.0.0-alpha08"
-    const val composeViewModel = "1.0.0-alpha02"
-    const val composeActivity = "1.3.0-alpha03"
+    const val compose = "1.0.0-beta02"
+    const val composeNav = "1.0.0-alpha09"
+    const val composeViewModel = "1.0.0-alpha03"
+    const val composeActivity = "1.3.0-alpha04"
 
-    const val buildGradle = "7.0.0-alpha08"
+    const val buildGradle = "7.0.0-alpha09"
 
     const val detekt = "1.13.1"
     const val ktlint = "0.39.0"

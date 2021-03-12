52d4f3eb8f09c36e5d0ac3de8b0614e1a13eee54
Igor Escodro <escodro@outlook.com>
2021-03-12 09:56:33 -0300

    ♻️ Refactor Task List BottomSheet (#148)
    Refactored the BottomSheet in order to make it more generic and avoiding
    passing states around once that hoisting it is better.

--- features/task/src/main/java/com/escodro/task/presentation/list/TaskStateHandler.kt
+++ features/task/src/main/java/com/escodro/task/presentation/list/TaskStateHandler.kt
@@ -6,4 +6,5 @@ internal data class TaskStateHandler(
     val state: TaskListViewState = TaskListViewState.Empty,
     val onCheckedChanged: (TaskWithCategory) -> Unit = {},
     val onItemClicked: (Long) -> Unit = {},
+    val onAddClicked: () -> Unit = {},
 )

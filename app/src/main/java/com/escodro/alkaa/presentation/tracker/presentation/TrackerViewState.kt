24bf3e84fba1cff09a6e40c11fd03874986e462b
Igor Escodro <escodro@outlook.com>
2021-04-05 09:12:35 -0300

    ✨ Adapt ViewModel to V2
    TrackerViewModel adapted from V1 to V2 to remove LiveData in favor of
    Flow and add better error handling.

--- app/src/main/java/com/escodro/alkaa/presentation/tracker/presentation/TrackerViewState.kt
+++ app/src/main/java/com/escodro/alkaa/presentation/tracker/presentation/TrackerViewState.kt
@@ -5,15 +5,20 @@ import com.escodro.alkaa.presentation.tracker.model.Tracker
 /**
  * Represents the possible UI States of Tracker screen.
  */
-internal sealed class TrackerUIState {
+internal sealed class TrackerViewState {
 
     /**
      * Represents the state where they are [Tracker.Info] to be shown on the screen.
      */
-    internal data class Loaded(val trackerInfo: Tracker.Info) : TrackerUIState()
+    internal data class Loaded(val trackerInfo: Tracker.Info) : TrackerViewState()
 
     /**
      * Represents the state where they are no information to be shown.
      */
-    internal object Empty : TrackerUIState()
+    internal object Empty : TrackerViewState()
+
+    /**
+     * Represents the state where an error occurred.
+     */
+    internal data class Error(val cause: Throwable) : TrackerViewState()
 }

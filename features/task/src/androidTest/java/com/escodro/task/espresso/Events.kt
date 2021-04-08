03a298734dcd7f48e506f7fb87ab3097f161aa6a
Igor Escodro <escodro@outlook.com>
2021-04-08 13:55:17 -0300

    ✅ Remove flakiness from AlarmSelectionTest (#157)
    Add the Test Rule to disable animations when using components from
    Android View System. Also a sleep was added in order to wait for the
    Android View dialog load. For sure it is not the best approach, however
    when a proper Compose dialog is launched, this problem will not happen.

--- features/task/src/androidTest/java/com/escodro/task/espresso/Events.kt
+++ features/task/src/androidTest/java/com/escodro/task/espresso/Events.kt
@@ -2,11 +2,14 @@ package com.escodro.task.espresso
 
 import com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setDateOnPicker
 import com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setTimeOnPicker
+import com.schibsted.spain.barista.interaction.BaristaSleepInteractions
 import java.util.Calendar
 
 internal fun setDateTime(calendar: Calendar) {
     with(calendar) {
+        BaristaSleepInteractions.sleep(300)
         setDateOnPicker(get(Calendar.YEAR), get(Calendar.MONTH) + 1, get(Calendar.DAY_OF_MONTH))
+        BaristaSleepInteractions.sleep(300)
         setTimeOnPicker(get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE))
     }
 }

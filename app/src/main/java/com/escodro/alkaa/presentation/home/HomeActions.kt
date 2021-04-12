e45f57b5113bc093fba0945155749cc5c59df92b
Igor Escodro <escodro@outlook.com>
2021-04-12 13:31:42 -0300

    ✨ Link the Navigation Graph action with Dynamic Delivery API
    The action when the users click on the Tracker feature now redirects to
    the Dynamic Delivery API check and opens the feature in the appropriate
    time.

--- app/src/main/java/com/escodro/alkaa/presentation/home/HomeActions.kt
+++ app/src/main/java/com/escodro/alkaa/presentation/home/HomeActions.kt
@@ -5,5 +5,6 @@ import com.escodro.alkaa.model.HomeSection
 internal data class HomeActions(
     val onTaskClick: (Long) -> Unit = {},
     val onAboutClick: () -> Unit = {},
+    val onTrackerClick: () -> Unit,
     val setCurrentSection: (HomeSection) -> Unit = {}
 )

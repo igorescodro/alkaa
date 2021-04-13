60d992ccf99817d3549d929117bc8c750467271c
Igor Escodro <escodro@outlook.com>
2021-04-13 13:46:49 -0300

    ✨ Create Tracker Task List
    New list create to identify each category and the numbers from each one.
    It works as caption as well.

--- features/tracker/src/main/java/com/escodro/tracker/presentation/TrackerSection.kt
+++ features/tracker/src/main/java/com/escodro/tracker/presentation/TrackerSection.kt
@@ -1,11 +1,17 @@
 package com.escodro.tracker.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.escodro.tracker.model.Tracker
 import com.escodro.tracker.presentation.components.TaskGraph
+import com.escodro.tracker.presentation.components.TaskTrackerList
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -30,5 +36,19 @@ internal fun TrackerContent(viewModel: TrackerViewModel = getViewModel()) {
 
 @Composable
 private fun TrackerLoadedContent(trackerInfo: Tracker.Info) {
-    TaskGraph(list = trackerInfo.categoryInfoList)
+    Column {
+        TaskGraph(
+            list = trackerInfo.categoryInfoList,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(2F)
+                .padding(24.dp)
+        )
+        TaskTrackerList(
+            list = trackerInfo.categoryInfoList,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+        )
+    }
 }

76fa91c93ea2174c78d18339fa64104f50914fdc
Igor Escodro <escodro@outlook.com>
2021-07-02 09:03:48 -0300

    🔖 Release Alkaa 2.0.0 final (#197)
    The stable version of Alkaa 2.0.0 is among us! ❤️

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 object Releases {
-    const val versionCode = 19002
-    const val versionName = "2.0.0-beta03"
+    const val versionCode = 20000
+    const val versionName = "2.0.0"
 }
 
 object Versions {

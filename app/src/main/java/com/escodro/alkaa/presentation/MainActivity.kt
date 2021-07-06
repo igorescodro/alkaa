14bc5599c9fc4e93d03bd1836ff55de2db87f1cd
Igor Escodro <escodro@outlook.com>
2021-07-06 15:08:49 -0300

    ♻️ Update from custom Back Press handler to API one
    Removing "LocalBackPressedDispatcher" in favor to the Handler already
    present in the API, the BackHandler.

--- app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
+++ app/src/main/java/com/escodro/alkaa/presentation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.escodro.alkaa.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.CompositionLocalProvider
-import com.escodro.alkaa.core.LocalBackPressedDispatcher
 import com.escodro.alkaa.navigation.NavGraph
 import com.escodro.designsystem.AlkaaTheme
 
@@ -17,11 +15,7 @@ internal class MainActivity : ComponentActivity() {
 
         setContent {
             AlkaaTheme {
-                CompositionLocalProvider(
-                    LocalBackPressedDispatcher provides onBackPressedDispatcher
-                ) {
-                    NavGraph()
-                }
+                NavGraph()
             }
         }
     }

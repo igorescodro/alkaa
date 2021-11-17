d441cc655896d24187f5815befba1319595196dc
Igor Escodro <escodro@outlook.com>
2021-11-17 09:31:22 -0300

    ✨ Update Theme to Material You
    Theme updated in order to reflect the Material You colors, typography
    and overall look and feel.

--- libraries/designsystem/src/main/java/com/escodro/designsystem/Theme.kt
+++ libraries/designsystem/src/main/java/com/escodro/designsystem/Theme.kt
@@ -1,39 +1,46 @@
 package com.escodro.designsystem
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
-    primary = blue700,
-    primaryVariant = blue900,
-    secondary = blueGray1000,
-    onSecondary = blueGray400
+private val AlkaaDarkColorScheme = darkColorScheme(
+    primary = blue700
 )
 
-private val LightColorPalette = lightColors(
-    primary = blue700,
-    primaryVariant = blue900,
-    secondary = blueGray1000,
-    onSecondary = blueGray400
+private val AlkaaLightColorScheme = lightColorScheme(
+    primary = blue700
 )
 
 /**
  * Alkaa main theme.
  *
- * @param darkTheme indicates if the application is in dark theme mode.
+ * @param isDarkTheme indicates if the application is in dark theme mode.
+ * @param isDynamicColor
  * @param content composable function
  */
 @Composable
-fun AlkaaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+fun AlkaaTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val dynamicColor = isDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && isDarkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !isDarkTheme -> dynamicLightColorScheme(LocalContext.current)
+        isDarkTheme -> AlkaaDarkColorScheme
+        else -> AlkaaLightColorScheme
+    }
 
     MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
+        colorScheme = colorScheme,
         content = content
     )
 }

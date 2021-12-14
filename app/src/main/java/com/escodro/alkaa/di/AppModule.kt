fa878e0266f3ce0af00df9109f5cc5ab739e8bbb
Igor Escodro <escodro@outlook.com>
2021-12-14 14:26:02 -0300

    ✨ Implement the Preferences in App Module
    Logic to load the "isDarkTheme" value from the DataStore now implemented
    in the App Module

--- app/src/main/java/com/escodro/alkaa/di/AppModule.kt
+++ app/src/main/java/com/escodro/alkaa/di/AppModule.kt
@@ -1,7 +1,10 @@
 package com.escodro.alkaa.di
 
+import com.escodro.alkaa.presentation.MainViewModel
+import com.escodro.alkaa.presentation.mapper.AppThemeOptionsMapper
 import com.escodro.core.extension.getAlarmManager
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
@@ -9,4 +12,8 @@ import org.koin.dsl.module
  */
 val appModule = module {
     factory { androidContext().getAlarmManager() }
+
+    viewModel { MainViewModel(get(), get()) }
+
+    factory { AppThemeOptionsMapper() }
 }

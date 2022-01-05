e929994e3e3a6fc8bf428dc2c1fc711a4e2885b3
Igor Escodro <escodro@outlook.com>
2022-01-05 08:39:07 -0300

    💄 Implement the Task List Glance/Widget
    A very simple Widget was implemented due to the limitations in the
    Glance library (which is in Alpha01). Some Composables and Modifiers are
    not yet available, not allowing more complex layouts. This UI may be
    revisited in the future.

--- features/glance/src/main/java/com/escodro/glance/presentation/TaskListGlanceWidget.kt
+++ features/glance/src/main/java/com/escodro/glance/presentation/TaskListGlanceWidget.kt
@@ -1,25 +1,41 @@
 package com.escodro.glance.presentation
 
 import android.content.Context
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.LocalGlanceId
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.appwidget.lazy.LazyColumn
+import androidx.glance.appwidget.lazy.items
 import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
-import androidx.glance.text.FontWeight
+import androidx.glance.layout.size
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import com.escodro.glance.R
 import com.escodro.glance.model.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
@@ -38,10 +54,11 @@ internal class TaskListGlanceWidget : GlanceAppWidget(), KoinComponent {
 
     private val coroutineScope: CoroutineScope = MainScope()
 
-    private var data by mutableStateOf<List<Task>>(emptyList())
+    private var taskList by mutableStateOf<List<Task>>(emptyList())
 
     private var glanceId by mutableStateOf<GlanceId?>(null)
 
+    @OptIn(ExperimentalUnitApi::class)
     @Composable
     override fun Content() {
         glanceId = LocalGlanceId.current
@@ -49,13 +66,83 @@ internal class TaskListGlanceWidget : GlanceAppWidget(), KoinComponent {
         Column(
             modifier = GlanceModifier
                 .fillMaxSize()
-                .background(color = Color.White)
+                .cornerRadius(12.dp)
+                .appWidgetBackground()
+                .background(color = MaterialTheme.colors.background)
                 .padding(8.dp)
         ) {
+            Row(
+                modifier = GlanceModifier.fillMaxWidth().height(32.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    provider = ImageProvider(R.drawable.ic_alkaa_icon),
+                    contentDescription = "",
+                    modifier = GlanceModifier.size(32.dp)
+                )
+                // Text(
+                //     text = taskList.size.toString(),
+                //     modifier = GlanceModifier.height(24.dp),
+                //     style = TextStyle(
+                //         color = ColorProvider(Color.DarkGray),
+                //         fontSize = TextUnit(22f, TextUnitType.Sp),
+                //         fontWeight = FontWeight.Bold
+                //     ),
+                //     maxLines = 1
+                // )
+            }
+            if (taskList.isEmpty()) {
+                EmptyListContent()
+            } else {
+                TaskListContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun EmptyListContent() {
+        Box(modifier = GlanceModifier.fillMaxSize().padding(top = 16.dp)) {
+            Text(text = "No tasks")
+        }
+    }
+
+    @Composable
+    private fun TaskListContent() {
+        LazyColumn(modifier = GlanceModifier.padding(top = 12.dp)) {
+            items(items = taskList) { task ->
+                TaskItem(task = task)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalUnitApi::class)
+    @Suppress("MagicNumber")
+    @Composable
+    private fun TaskItem(task: Task) {
+        Column(
+            modifier = GlanceModifier
+                .height(32.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text(
-                text = data.firstOrNull()?.title ?: "Testing",
-                modifier = GlanceModifier.fillMaxWidth(),
-                style = TextStyle(fontWeight = FontWeight.Bold),
+                text = task.title,
+                modifier = GlanceModifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+                    .height(24.dp),
+                style = TextStyle(
+                    color = ColorProvider(Color.DarkGray),
+                    fontSize = TextUnit(14f, TextUnitType.Sp),
+                ),
+                maxLines = 1
+            )
+            Box(
+                GlanceModifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(color = Color.DarkGray),
+                content = {}
             )
         }
     }
@@ -70,8 +157,7 @@ internal class TaskListGlanceWidget : GlanceAppWidget(), KoinComponent {
     fun loadData() {
         println("loadData")
         coroutineScope.launch {
-            data = viewModel.loadTaskList().first()
-
+            taskList = viewModel.loadTaskList().first()
             val currentGlanceId = snapshotFlow { glanceId }.firstOrNull()
 
             if (currentGlanceId != null) {

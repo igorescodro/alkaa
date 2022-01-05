d17d9e02869ac6b7e1abd16ed0b1ccc6622cd2cb
Igor Escodro <escodro@outlook.com>
2022-01-05 09:05:29 -0300

    ✨ Implement Glance Interactor
    A new interactor was implemented in the :domain layer in order to notify
    the :glance feature when a significant change occurred in the task item
    or in the list, reflecting it in the widget.

--- domain/src/main/java/com/escodro/domain/usecase/task/implementation/AddTaskImpl.kt
+++ domain/src/main/java/com/escodro/domain/usecase/task/implementation/AddTaskImpl.kt
@@ -1,11 +1,15 @@
 package com.escodro.domain.usecase.task.implementation
 
+import com.escodro.domain.interactor.GlanceInteractor
 import com.escodro.domain.model.Task
 import com.escodro.domain.repository.TaskRepository
 import com.escodro.domain.usecase.task.AddTask
 import mu.KLogging
 
-internal class AddTaskImpl(private val taskRepository: TaskRepository) : AddTask {
+internal class AddTaskImpl(
+    private val taskRepository: TaskRepository,
+    private val glanceInteractor: GlanceInteractor
+) : AddTask {
 
     override suspend operator fun invoke(task: Task) {
         if (task.title.isBlank()) {
@@ -14,6 +18,7 @@ internal class AddTaskImpl(private val taskRepository: TaskRepository) : AddTask
         }
 
         taskRepository.insertTask(task)
+        glanceInteractor.onTaskListUpdated()
     }
 
     companion object : KLogging()
--- features/glance/src/main/java/com/escodro/glance/di/GlanceModule.kt
+++ features/glance/src/main/java/com/escodro/glance/di/GlanceModule.kt
@@ -1,7 +1,10 @@
 package com.escodro.glance.di
 
+import com.escodro.domain.interactor.GlanceInteractor
+import com.escodro.glance.interactor.GlanceInteractorImpl
 import com.escodro.glance.mapper.TaskMapper
 import com.escodro.glance.presentation.TaskListGlanceDataLoader
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 /**
@@ -11,6 +14,9 @@ val glanceModule = module {
 
     // Presentation
     single { TaskListGlanceDataLoader(get(), get()) }
+
+    // Interactor
+    factory<GlanceInteractor> { GlanceInteractorImpl(androidContext()) }
 
     // Mapper
     factory { TaskMapper() }

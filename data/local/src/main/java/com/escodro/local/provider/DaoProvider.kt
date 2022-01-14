28c0be6e00c8efa1911582cba802698260c74e28
Igor Escodro <escodro@outlook.com>
2022-01-14 15:05:05 -0300

    ♻️ Remove GlobalScope references from the codebase
    Instead of using GlobalScope, now Alkaa was updated to use an app based
    CoroutineScope for more flexibility and control.

--- data/local/src/main/java/com/escodro/local/provider/DaoProvider.kt
+++ data/local/src/main/java/com/escodro/local/provider/DaoProvider.kt
@@ -7,7 +7,7 @@ import com.escodro.local.dao.TaskWithCategoryDao
 /**
  * Repository with the database [androidx.room.Dao]s.
  */
-class DaoProvider(private val database: DatabaseProvider) {
+internal class DaoProvider(private val database: DatabaseProvider) {
 
     /**
      * Gets the [TaskDao].

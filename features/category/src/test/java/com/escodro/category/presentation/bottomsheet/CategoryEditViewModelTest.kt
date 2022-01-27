71cf184d1c7cf6323ab51c4d19027ba61e5a4b6d
Igor Escodro <escodro@outlook.com>
2022-01-27 09:11:34 -0300

    ✅ Add and update BottomSheet-related tests
    Some changes were required in the BottomSheet ViewModels, so new tests
    were added and other ones were updated with new interface reference

--- features/category/src/test/java/com/escodro/category/presentation/bottomsheet/CategoryEditViewModelTest.kt
+++ features/category/src/test/java/com/escodro/category/presentation/bottomsheet/CategoryEditViewModelTest.kt
@@ -2,14 +2,22 @@ package com.escodro.category.presentation.bottomsheet
 
 import android.graphics.Color
 import com.escodro.category.fake.DeleteCategoryFake
+import com.escodro.category.fake.FAKE_DOMAIN_CATEGORY
+import com.escodro.category.fake.LoadCategoryFake
 import com.escodro.category.fake.UpdateCategoryFake
 import com.escodro.category.mapper.CategoryMapper
 import com.escodro.categoryapi.model.Category
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class CategoryEditViewModelTest {
+
+    private val loadCategoryFake = LoadCategoryFake()
 
     private val updateCategory = UpdateCategoryFake()
 
@@ -17,12 +25,39 @@ internal class CategoryEditViewModelTest {
 
     private val mapper = CategoryMapper()
 
-    private val viewModel = CategoryEditViewModel(updateCategory, deleteCategory, mapper)
+    private val viewModel =
+        CategoryEditViewModel(loadCategoryFake, updateCategory, deleteCategory, mapper)
 
     @Before
     fun setup() {
         updateCategory.clear()
         deleteCategory.clear()
+    }
+
+    @Test
+    fun `test if category is loaded when the id is valid`() = runBlockingTest {
+        // Given the task is correctly returned
+        loadCategoryFake.categoryToBeReturned = FAKE_DOMAIN_CATEGORY
+        val flow = viewModel.loadCategory(FAKE_DOMAIN_CATEGORY.id)
+
+        // When the latest event is collected
+        val state = flow.first()
+
+        // Then the state contain the loaded category
+        require(state is CategorySheetState.Loaded)
+        val assertCategoryView = mapper.toView(FAKE_DOMAIN_CATEGORY)
+        Assert.assertEquals(assertCategoryView, state.category)
+    }
+
+    @Test
+    fun `test if category is empty when the id is not valid`() = runBlockingTest {
+        // Given the task id is invalid
+        loadCategoryFake.categoryToBeReturned = null
+        val flow = viewModel.loadCategory(666L)
+
+        val state = flow.first()
+
+        Assert.assertTrue(state is CategorySheetState.Empty)
     }
 
     @Test

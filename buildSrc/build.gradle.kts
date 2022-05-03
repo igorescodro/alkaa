8b7947a78e75e258c0aeb5ea2e3fbb6f2d96ac8d
Igor Escodro <escodro@outlook.com>
2022-05-03 13:07:07 -0300

    ⬆️ Upgrade Gradle-related dependencies to latest version
    In order to match latest Android Studio Dolphin, the dependencies were
    updated!

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0-alpha07")
+    implementation("com.android.tools.build:gradle:7.3.0-alpha09")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")

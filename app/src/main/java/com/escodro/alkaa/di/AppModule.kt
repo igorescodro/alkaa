589e4df8c8d78d4b21a30cd0bccc84525cf7c5ac
Igor Escodro <escodro@outlook.com>
2022-08-11 14:13:05 -0400

    ✨ Add new PermissionChecker interface
    New interface implemented to delegate the permission check, being able
    to better unit testing the functionallity.

--- app/src/main/java/com/escodro/alkaa/di/AppModule.kt
+++ app/src/main/java/com/escodro/alkaa/di/AppModule.kt
@@ -2,10 +2,8 @@ package com.escodro.alkaa.di
 
 import com.escodro.alkaa.presentation.MainViewModel
 import com.escodro.alkaa.presentation.mapper.AppThemeOptionsMapper
-import com.escodro.core.extension.getAlarmManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,8 +11,6 @@ import org.koin.dsl.module
  * Application module.
  */
 val appModule = module {
-    factory { androidContext().getAlarmManager() }
-
     factory { CoroutineScope(SupervisorJob()) }
 
     viewModel { MainViewModel(get(), get()) }
--- features/alarm/src/test/java/com/escodro/alarm/AlarmPermissionTest.kt
+++ features/alarm/src/test/java/com/escodro/alarm/AlarmPermissionTest.kt
@@ -1,25 +1,30 @@
 package com.escodro.alarm
 
-import android.app.AlarmManager
 import android.os.Build
 import com.escodro.alarm.fake.AndroidVersionFake
+import com.escodro.alarm.fake.PermissionCheckerFake
 import com.escodro.alarm.permission.AlarmPermissionImpl
-import io.mockk.every
-import io.mockk.mockk
+import org.junit.After
 import org.junit.Assert
 import org.junit.Test
 
 internal class AlarmPermissionTest {
 
-    private val alarmManager = mockk<AlarmManager>(relaxed = true)
+    private val permissionChecker = PermissionCheckerFake()
 
     private val androidVersion = AndroidVersionFake()
 
-    private val alarmPermission = AlarmPermissionImpl(alarmManager, androidVersion)
+    private val alarmPermission = AlarmPermissionImpl(permissionChecker, androidVersion)
+
+    @After
+    fun tearDown() {
+        permissionChecker.clean()
+        androidVersion.clean()
+    }
 
     @Test
-    fun `test if when permission is granted returns true`() {
-        every { alarmManager.canScheduleExactAlarms() } returns true
+    fun `test when exact alarm permission is granted then returns true`() {
+        permissionChecker.hasExactAlarmsPermission = true
         androidVersion.version = Build.VERSION_CODES.S
 
         val result = alarmPermission.hasExactAlarmPermission()
@@ -28,8 +33,8 @@ internal class AlarmPermissionTest {
     }
 
     @Test
-    fun `test if when permission is not granted returns false`() {
-        every { alarmManager.canScheduleExactAlarms() } returns false
+    fun `test when exact alarm permission is not granted then returns false`() {
+        permissionChecker.hasExactAlarmsPermission = false
         androidVersion.version = Build.VERSION_CODES.S
 
         val result = alarmPermission.hasExactAlarmPermission()
@@ -38,10 +43,41 @@ internal class AlarmPermissionTest {
     }
 
     @Test
-    fun `test if Android below S returns true`() {
+    fun `test when exact alarm on Android below S then returns true`() {
+        permissionChecker.hasExactAlarmsPermission = false // Permission only available >= S
         androidVersion.version = Build.VERSION_CODES.M
 
         val result = alarmPermission.hasExactAlarmPermission()
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `test when notification permission is granted then returns true`() {
+        permissionChecker.hasPermission = true
+        androidVersion.version = Build.VERSION_CODES.TIRAMISU
+
+        val result = alarmPermission.hasNotificationPermission()
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `test when notification permission is not granted then returns false`() {
+        permissionChecker.hasPermission = false
+        androidVersion.version = Build.VERSION_CODES.TIRAMISU
+
+        val result = alarmPermission.hasNotificationPermission()
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `test when notification permission on Android below T then returns true`() {
+        permissionChecker.hasPermission = false // Permission only available >= S
+        androidVersion.version = Build.VERSION_CODES.S
+
+        val result = alarmPermission.hasNotificationPermission()
 
         Assert.assertTrue(result)
     }

7bd6722b7a6cbefae248cce2a157bd95f2c1766c
Igor Escodro <escodro@outlook.com>
2022-12-15 15:49:50 -0500

    🔖 Release Alkaa 2.2.4 (#394)
    New version released

--- plugins/src/main/java/AlkaaVersions.kt
+++ plugins/src/main/java/AlkaaVersions.kt
@@ -6,8 +6,8 @@ val VersionCatalog.composeVersion: String
     get() = findVersion("compose_compiler").get().requiredVersion
 
 object AlkaaVersions {
-    const val versionCode = 20203
-    const val versionName = "2.2.3"
+    const val versionCode = 20204
+    const val versionName = "2.2.4"
 
     const val compileSdk = 33
     const val targetSdk = 33

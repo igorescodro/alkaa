6a7fdca458ad2c042c9c9155217b6d9eba22d60c
Igor Escodro <escodro@outlook.com>
2023-04-25 09:50:40 -0400

    🚨 Add ChecksSdkIntAtLeast annotation due to Android Lint
    The annotation was added to comply with new Lint rules

--- features/alarm/src/main/java/com/escodro/alarm/permission/AndroidVersionImpl.kt
+++ features/alarm/src/main/java/com/escodro/alarm/permission/AndroidVersionImpl.kt
@@ -1,8 +1,10 @@
 package com.escodro.alarm.permission
 
 import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
 
 internal class AndroidVersionImpl : AndroidVersion {
 
+    @ChecksSdkIntAtLeast(extension = Build.VERSION_CODES.N)
     override val currentVersion: Int = Build.VERSION.SDK_INT
 }

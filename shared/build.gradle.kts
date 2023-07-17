bc735290e95a2fe9d4a9c08aaf0fe0691a3fb577
Igor Escodro <escodro@outlook.com>
2023-07-17 09:45:42 -0400

    🚚 Create new KMM shared module and move the Domain logic there
    A new `shared` module was create to hold all the shared logic between
    the different mobile platforms. Future changes will make the `shared`
    module the new `domain` if possible.
    
    Since the `:domain` module is already Kotlin-only, the code was simply
    moved and works!

---
+++ shared/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.koin.core)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.logging)
+                implementation(libs.logback)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.escodro.shared"
+    compileSdk = 33 // TODO move the versions to TOML and use it here
+    defaultConfig {
+        minSdk = 24 // TODO move the versions to TOML and use it here
+    }
+}

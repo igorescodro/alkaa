d4a4428ea1aede30232e20944eb69276a76ed741
Igor Escodro <escodro@outlook.com>
2023-08-08 08:58:29 -0400

    ♻️ Update the Repository module to KMP
    The Repository module was migrated from Android/Kotlin-only module to
    KMP. For now, we are not caring too much about ProGuard, so the files
    were removed.

--- data/repository/build.gradle.kts
+++ data/repository/build.gradle.kts
@@ -1,16 +1,53 @@
 plugins {
-    id("com.escodro.android-library") // TODO revert to Kotlin-only
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("com.escodro.kotlin-quality")
 }
 
-dependencies {
-    implementation(projects.domain)
-    implementation(projects.libraries.core)
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
 
-    implementation(libs.koin.core)
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.datetime)
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "repository"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.domain)
+
+                implementation(libs.koin.core)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinx.datetime)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
 }
 
 android {
     namespace = "com.escodro.repository"
+    compileSdk = Integer.parseInt(libs.versions.android.sdk.compile.get())
+    defaultConfig {
+        minSdk = Integer.parseInt(libs.versions.android.sdk.min.get())
+    }
 }

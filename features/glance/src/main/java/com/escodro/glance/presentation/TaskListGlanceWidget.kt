ad7534f744c644288e8eba935b4ad438d00193dd
tuuhin <tuhinbhowmick2513@gmail.com>
2023-08-11 00:35:55 +0530

    Added theming in the glance widget AlkaaGlanceTheme provides the glance theme,its used to get the dynamic colors,providing the color-provider by ColorProviders on Android 12+ dynamic color is taken in using material3 glance dependency and below that designed system colors. Under Android 12 glance docent provides corner radius thus a shape drawable with the similar color radius  is implemented with surface colors. Additionally due to the theme change preview is not properly visible in the widget drawer so added surface and surface color values For No task additional vector resource is used to designate there is no tasks. The heading of the widget was looking blank so added the Heading "Your Tasks",for the portuguese I just googled the translation,please change them if they are wrong. TaskListGlanceWidget changes are added, changes include use of GlanceTheme.colors provider as color provider and some others. In loading and preview added the background which is the same drawable mentioned above

--- features/glance/src/main/java/com/escodro/glance/presentation/TaskListGlanceWidget.kt
+++ features/glance/src/main/java/com/escodro/glance/presentation/TaskListGlanceWidget.kt
@@ -2,20 +2,21 @@ package com.escodro.glance.presentation
 
 import android.content.Context
 import android.content.Intent
-import androidx.compose.material3.MaterialTheme
+import android.os.Build
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
+import androidx.glance.LocalContext
 import androidx.glance.action.actionParametersOf
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.CheckBox
+import androidx.glance.appwidget.CheckboxDefaults
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.action.actionStartActivity
@@ -27,27 +28,27 @@ import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.layout.size
+import androidx.glance.layout.width
 import androidx.glance.state.GlanceStateDefinition
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
-import androidx.glance.unit.ColorProvider
 import com.escodro.glance.R
 import com.escodro.glance.data.TaskListStateDefinition
 import com.escodro.glance.model.Task
 import com.escodro.navigation.DestinationDeepLink
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
-import org.koin.core.component.KoinComponent
 
-internal class TaskListGlanceWidget : GlanceAppWidget(), KoinComponent {
+internal class TaskListGlanceWidget : GlanceAppWidget() {
 
     /**
      * Custom implementation of [GlanceStateDefinition] to save and store data for this widget.
@@ -57,86 +58,136 @@ internal class TaskListGlanceWidget : GlanceAppWidget(), KoinComponent {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) = provideContent {
         val taskList = currentState<List<Task>>().toImmutableList()
-        Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .cornerRadius(12.dp)
-                .appWidgetBackground()
-                .background(color = MaterialTheme.colorScheme.background)
-                .padding(8.dp),
-        ) {
-            Row(
-                modifier = GlanceModifier.fillMaxWidth().height(32.dp),
-                verticalAlignment = Alignment.CenterVertically,
+
+        /**
+         * As corner radius is only available from Android 12+
+         * For lower versions adding a background with a  filled shape of same
+         * background color and corner radius.
+         */
+        val background = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            GlanceModifier
+                .cornerRadius(10.dp)
+                .background(GlanceTheme.colors.background)
+        else GlanceModifier.background(ImageProvider(R.drawable.rounded_corners))
+
+        AlkaaGlanceTheme {
+            Column(
+                modifier = GlanceModifier
+                    .appWidgetBackground()
+                    .fillMaxSize()
+                    .then(background)
+                    .padding(8.dp),
             ) {
-                Image(
-                    provider = ImageProvider(R.drawable.ic_alkaa_icon),
-                    contentDescription = "",
-                    modifier = GlanceModifier.size(32.dp)
-                        .clickable(actionStartActivity(getHomeIntent())),
-                )
+                Row(
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .height(32.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Image(
+                        provider = ImageProvider(R.drawable.ic_alkaa_icon),
+                        contentDescription = "",
+                        modifier = GlanceModifier
+                            .size(32.dp)
+                            .clickable(actionStartActivity(getHomeIntent())),
+                    )
+                    Spacer(modifier = GlanceModifier.width(12.dp))
+                    Text(
+                        text = context.getString(R.string.glance_heading),
+                        style = TextStyle(
+                            color = GlanceTheme.colors.onSurface,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium
+                        ),
+                        modifier = GlanceModifier
+                            .defaultWeight()
+                    )
+                }
+                if (taskList.isEmpty()) {
+                    EmptyListContent()
+                } else {
+                    TaskListContent(taskList = taskList)
+                }
             }
-            if (taskList.isEmpty()) {
-                EmptyListContent()
-            } else {
-                TaskListContent(taskList = taskList)
-            }
+        }
+
+    }
+
+    @Composable
+    private fun EmptyListContent(
+        context: Context = LocalContext.current,
+        modifier: GlanceModifier = GlanceModifier
+    ) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                provider = ImageProvider(R.drawable.ic_task_list),
+                contentDescription = "Blank Task List",
+                colorFilter = ColorFilter
+                    .tint(GlanceTheme.colors.secondary),
+                modifier = GlanceModifier.size(48.dp)
+            )
+            Spacer(modifier = GlanceModifier.height(12.dp))
+            Text(
+                text = context.getString(R.string.glance_no_tasks),
+                style = TextStyle(
+                    color = GlanceTheme.colors.onSurface,
+                    fontSize = 12.sp,
+                ),
+            )
         }
     }
 
     @Composable
-    private fun EmptyListContent() {
-        Box(modifier = GlanceModifier.fillMaxSize().padding(top = 16.dp)) {
-            Text(text = "No tasks")
-        }
-    }
-
-    @Composable
-    private fun TaskListContent(taskList: ImmutableList<Task>) {
-        LazyColumn(modifier = GlanceModifier.padding(top = 12.dp)) {
+    private fun TaskListContent(
+        taskList: ImmutableList<Task>,
+        modifier: GlanceModifier = GlanceModifier
+    ) {
+        LazyColumn(
+            modifier = modifier.padding(top = 8.dp)
+        ) {
             items(items = taskList) { task ->
                 TaskItem(task = task)
             }
         }
     }
 
-    @OptIn(ExperimentalUnitApi::class)
     @Suppress("MagicNumber")
     @Composable
-    private fun TaskItem(task: Task) {
-        Column(
-            modifier = GlanceModifier
-                .height(32.dp)
+    private fun TaskItem(task: Task, modifier: GlanceModifier = GlanceModifier) {
+        Row(
+            modifier = modifier
+                .padding(vertical = 2.dp)
                 .fillMaxWidth()
                 .clickable(actionStartActivity(getTaskIntent(task.id))),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Row(
-                modifier = GlanceModifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                CheckBox(
-                    checked = false,
-                    onCheckedChange = actionRunCallback<UpdateTaskStatusAction>(
-                        actionParametersOf(TaskIdKey to task.id.toString()),
-                    ),
-                    modifier = GlanceModifier.size(32.dp),
-                )
-                Text(
-                    text = task.title,
-                    modifier = GlanceModifier
-                        .padding(start = 4.dp, end = 8.dp)
-                        .fillMaxWidth()
-                        .height(24.dp),
-                    style = TextStyle(
-                        color = ColorProvider(Color.DarkGray),
-                        fontSize = TextUnit(14f, TextUnitType.Sp),
-                    ),
-                    maxLines = 1,
-                )
-            }
+            CheckBox(
+                checked = false,
+                onCheckedChange = actionRunCallback<UpdateTaskStatusAction>(
+                    actionParametersOf(TaskIdKey to task.id.toString()),
+                ),
+                modifier = GlanceModifier.size(32.dp),
+                colors = CheckboxDefaults.colors(
+                    checkedColor = GlanceTheme.colors.secondary,
+                    uncheckedColor = GlanceTheme.colors.onSurface
+                ),
+            )
+            Spacer(modifier = GlanceModifier.width(2.dp))
+            Text(
+                text = task.title,
+                style = TextStyle(color = GlanceTheme.colors.onSurface, fontSize = 14.sp),
+                maxLines = 1,
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .height(24.dp),
+            )
         }
     }
+
 
     private fun getHomeIntent(): Intent =
         Intent(Intent.ACTION_VIEW, DestinationDeepLink.getTaskHomeUri())

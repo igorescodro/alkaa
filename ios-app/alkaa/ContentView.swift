b88958a88515b7c8054dcd3d64b34bcaceb8049c
Igor Escodro <escodro@outlook.com>
2023-08-18 09:42:21 -0400

    🍏 Create basic iOS project
    Basic iOS project created linking it with the KMM generated files.
    
    More info: https://kotlinlang.org/docs/multiplatform-mobile-integrate-in-existing-app.html#connect-the-framework-to-your-ios-project

---
+++ ios-app/alkaa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alkaa
+//
+//  Created by Igor Escodro on 2023-08-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-app/alkaa/alkaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  alkaaApp.swift
+//  alkaa
+//
+//  Created by Igor Escodro on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct alkaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

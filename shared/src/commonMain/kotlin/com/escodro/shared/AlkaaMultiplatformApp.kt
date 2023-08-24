e3f7c2285e72450bbfa75cc9c0909b692de02063
Igor Escodro <escodro@outlook.com>
2023-08-24 16:20:34 -0400

    💄 Replace generic theme with AlkaaTheme
    Theme in `shared` replaced with the atual Alkaa Material Design 3 theme

--- shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
@@ -1,23 +1,28 @@
 package com.escodro.shared
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.escodro.designsystem.AlkaaTheme
 
 @Composable
 fun AlkaaMultiplatformApp() {
-    MaterialTheme {
+    AlkaaTheme {
         HelloWorld()
     }
 }
 
 @Composable
 private fun HelloWorld() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Hello World!")
+    Box(
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.surface),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = "Hello World!", color = MaterialTheme.colorScheme.onBackground)
     }
 }

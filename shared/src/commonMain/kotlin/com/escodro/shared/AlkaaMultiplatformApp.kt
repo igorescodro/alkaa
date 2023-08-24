e1a9f1472962683e9aa008425496bde0774a3275
Igor Escodro <escodro@outlook.com>
2023-08-24 11:57:29 -0400

    ✨ Add a Hello World composable in shared
    A new simple Composable was added to make sure that the iOS -> KMP
    integration is working properly.

--- shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
@@ -1,11 +1,23 @@
 package com.escodro.shared
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun AlkaaMultiplatformApp() {
     MaterialTheme {
-        // Shine!
+        HelloWorld()
+    }
+}
+
+@Composable
+private fun HelloWorld() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Hello World!")
     }
 }

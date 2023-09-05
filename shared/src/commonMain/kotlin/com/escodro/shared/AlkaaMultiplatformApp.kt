cba084f48772c73020b7e802ed338af966539bb4
Igor Escodro <escodro@outlook.com>
2023-09-05 09:25:46 -0400

    ♻️ Update the Shared module to expose the strings for iOS
    Simple update in the Shared module to expose the resources from the
    "Resources" module.

--- shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
@@ -1,14 +1,8 @@
 package com.escodro.shared
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.escodro.designsystem.AlkaaTheme
+import com.escodro.home.presentation.Home
 
 @Composable
 fun AlkaaMultiplatformApp() {
@@ -19,10 +13,12 @@ fun AlkaaMultiplatformApp() {
 
 @Composable
 private fun HelloWorld() {
-    Box(
-        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.surface),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(text = "Hello World!", color = MaterialTheme.colorScheme.onBackground)
-    }
+    Home(
+        onTaskClick = {},
+        onAboutClick = {},
+        onTrackerClick = {},
+        onOpenSourceClick = {},
+        onTaskSheetOpen = {},
+        onCategorySheetOpen = {},
+    )
 }

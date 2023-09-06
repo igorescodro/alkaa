0b0837a29bb808a03d4c10e5b5c3390da647f45c
Igor Escodro <escodro@outlook.com>
2023-09-06 10:02:05 -0400

    ♻️ Convert `category-api` to KMP
    API module converted to KMP

--- features/category-api/build.gradle.kts
+++ features/category-api/build.gradle.kts
@@ -1,13 +1,25 @@
+import extension.commonDependencies
+import extension.setFrameworkBaseName
+
 plugins {
-    id("com.escodro.android-library")
+    id("com.escodro.multiplatform")
     id("kotlin-parcelize")
 }
 
-dependencies {
-    api(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.collections.immutable)
+kotlin {
+    setFrameworkBaseName("categoryapi")
+
+    commonDependencies {
+        api(libs.androidx.lifecycle.viewmodel)
+        implementation(libs.kotlinx.coroutines.core)
+        implementation(libs.kotlinx.collections.immutable)
+        implementation(libs.moko.parcelize)
+    }
 }
+
 android {
     namespace = "com.escodro.categoryapi"
 }
+
+
+

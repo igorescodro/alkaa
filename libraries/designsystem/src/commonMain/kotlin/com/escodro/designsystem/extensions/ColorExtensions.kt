88501ff8e382d05f96c52509e21ae47fe3934898
Igor Escodro <escodro@outlook.com>
2023-09-19 13:37:31 -0400

    🚚 Move color extension
    We are killing `libraries:core` step by step because it's a big utils
    class (and heavily Android-dependant). At this time, we are moving the
    color extension to `libraries:designsystem`.

---
+++ libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/extensions/ColorExtensions.kt
@@ -0,0 +1,16 @@
+package com.escodro.designsystem.extensions
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+
+/**
+ * Converts a hex color string to ARGB color.
+ *
+ * @return the ARGB color
+ */
+fun String.toArgbColor(): Int {
+    val hashColorString = this
+    return Color(
+        hashColorString.removePrefix("#").toLong(16) or 0x00000000FF000000,
+    ).toArgb()
+}

e8efe9cff3ea37cd09f16851fa55575a0113c45f
Igor Escodro <escodro@outlook.com>
2023-09-22 10:42:34 -0400

    ♻️ Update the Shared module
    The Shared feature was updated to be more aligned with the new
    navigation system

--- shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/escodro/shared/AlkaaMultiplatformApp.kt
@@ -2,23 +2,11 @@ package com.escodro.shared
 
 import androidx.compose.runtime.Composable
 import com.escodro.designsystem.AlkaaTheme
-import com.escodro.home.presentation.Home
+import com.escodro.shared.navigation.AlkaaNavGraph
 
 @Composable
 fun AlkaaMultiplatformApp() {
     AlkaaTheme {
-        HelloWorld()
+        AlkaaNavGraph()
     }
-}
-
-@Composable
-private fun HelloWorld() {
-    Home(
-        onTaskClick = {},
-        onAboutClick = {},
-        onTrackerClick = {},
-        onOpenSourceClick = {},
-        onTaskSheetOpen = {},
-        onCategorySheetOpen = {},
-    )
 }

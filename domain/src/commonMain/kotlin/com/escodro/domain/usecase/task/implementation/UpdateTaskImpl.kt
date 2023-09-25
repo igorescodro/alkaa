df517de416570220dd63bae9bc0078b5bb4ca046
Igor Escodro <escodro@outlook.com>
2023-09-25 14:10:44 -0400

    🚧 Fix the unit tests
    Temporary solution to fix domain the unit tests. Since the Notification,
    Alarm and Widget/Glance features are not ported to KMM, the DI will
    provide a nullable injection for those Interactors. Unfortunately, Koin
    does not support `getOrNull` with the Constructor DSL so we are back
    to the ugly injection temporarily.

--- domain/src/commonMain/kotlin/com/escodro/domain/usecase/task/implementation/UpdateTaskImpl.kt
+++ domain/src/commonMain/kotlin/com/escodro/domain/usecase/task/implementation/UpdateTaskImpl.kt
@@ -1,5 +1,6 @@
 package com.escodro.domain.usecase.task.implementation
 
+import com.escodro.domain.interactor.GlanceInteractor
 import com.escodro.domain.model.Task
 import com.escodro.domain.repository.TaskRepository
 import com.escodro.domain.usecase.task.UpdateTask
@@ -9,11 +10,11 @@ import com.escodro.domain.usecase.task.UpdateTask
  */
 internal class UpdateTaskImpl(
     private val taskRepository: TaskRepository,
-    // private val glanceInteractor: GlanceInteractor, TODO
+    private val glanceInteractor: GlanceInteractor?,
 ) : UpdateTask {
 
     override suspend operator fun invoke(task: Task) {
         taskRepository.updateTask(task)
-        // glanceInteractor.onTaskListUpdated()
+        glanceInteractor?.onTaskListUpdated()
     }
 }
--- domain/src/commonMain/kotlin/com/escodro/domain/usecase/task/implementation/UpdateTaskTitleImpl.kt
+++ domain/src/commonMain/kotlin/com/escodro/domain/usecase/task/implementation/UpdateTaskTitleImpl.kt
@@ -1,5 +1,6 @@
 package com.escodro.domain.usecase.task.implementation
 
+import com.escodro.domain.interactor.GlanceInteractor
 import com.escodro.domain.usecase.task.LoadTask
 import com.escodro.domain.usecase.task.UpdateTask
 import com.escodro.domain.usecase.task.UpdateTaskTitle
@@ -7,13 +8,13 @@ import com.escodro.domain.usecase.task.UpdateTaskTitle
 internal class UpdateTaskTitleImpl(
     private val loadTask: LoadTask,
     private val updateTask: UpdateTask,
-    // private val glanceInteractor: GlanceInteractor, // TODO
+    private val glanceInteractor: GlanceInteractor?,
 ) : UpdateTaskTitle {
 
     override suspend fun invoke(taskId: Long, title: String) {
         val task = loadTask(taskId) ?: return
         val updatedTask = task.copy(title = title)
         updateTask(updatedTask)
-        // glanceInteractor.onTaskListUpdated()
+        glanceInteractor?.onTaskListUpdated()
     }
 }

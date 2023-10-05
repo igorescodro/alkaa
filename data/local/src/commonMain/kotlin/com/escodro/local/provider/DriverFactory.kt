e82bafbbb26172b3abc381f442037d7a0ad6e270
Igor Escodro <escodro@outlook.com>
2023-10-05 10:28:19 -0400

    ✨ Introduce category pre-population on iOS
    Since the SQLDelight does have a callback when the database schema is
    created, the solution was checking if the database is created before
    inserting the categories. The library creates the database on a specific
    folder, which is now checked on iOS variant.
    
    This change also required a few structure changes, such as the place of
    the strings and implementing the resource access on each platform.

--- data/local/src/commonMain/kotlin/com/escodro/local/provider/DriverFactory.kt
+++ data/local/src/commonMain/kotlin/com/escodro/local/provider/DriverFactory.kt
@@ -1,11 +1,35 @@
 package com.escodro.local.provider
 
 import app.cash.sqldelight.db.SqlDriver
-import com.escodro.local.AlkaaDatabase
+import com.escodro.local.Category
 
-interface DriverFactory {
+/**
+ * Provides the platform-specific [SqlDriver] to be used in the database.
+ */
+internal interface DriverFactory {
+
+    /**
+     * Creates the platform-specific [SqlDriver] to be used in the database.
+     *
+     * @param databaseName the database name
+     *
+     * @return the [SqlDriver] to be used in the database
+     */
     fun createDriver(databaseName: String): SqlDriver
 
-    // TODO better document here
-    fun prepopulateDatabase(database: AlkaaDatabase, databaseName: String)
+    /**
+     * Checks if the database is opening for the first time and should be prepopulated.
+     *
+     * @param databaseName the database name
+     *
+     * @return true if the database should be prepopulated, false otherwise
+     */
+    fun shouldPrepopulateDatabase(databaseName: String): Boolean
+
+    /**
+     * Gets the prepopulate data to be inserted in the database.
+     *
+     * @return the prepopulate data
+     */
+    fun getPrepopulateData(): List<Category>
 }

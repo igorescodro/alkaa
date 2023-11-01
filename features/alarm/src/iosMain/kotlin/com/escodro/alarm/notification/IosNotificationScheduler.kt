5b646de4f7c5495a8f766ba38b151981ddab064a
Igor Escodro <escodro@outlook.com>
2023-11-01 10:23:17 -0400

    🚧 Update the TODOs in IosNotificationScheduler
    Replace the function ones with the regular comment to avoid crashes
    while testing the DateTimePicker flow.

--- features/alarm/src/iosMain/kotlin/com/escodro/alarm/notification/IosNotificationScheduler.kt
+++ features/alarm/src/iosMain/kotlin/com/escodro/alarm/notification/IosNotificationScheduler.kt
@@ -3,10 +3,10 @@ package com.escodro.alarm.notification
 internal class IosNotificationScheduler : NotificationScheduler {
 
     override fun scheduleTaskNotification(taskId: Long, timeInMillis: Long) {
-        TODO("Not yet implemented")
+        // TODO
     }
 
     override fun cancelTaskNotification(taskId: Long) {
-        TODO("Not yet implemented")
+        // TODO
     }
 }

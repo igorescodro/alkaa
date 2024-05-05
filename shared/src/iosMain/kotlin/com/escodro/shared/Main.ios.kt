db7bd803c1e2cac424cd206e23987e5e6b3ed8bf
Igor Escodro <escodro@outlook.com>
2024-05-05 13:45:14 -0400

    ♿️ Add Accessibility support for Alkaa
    Adding accessibility sync options not only provides a better UX
    experience for all users but also allows Maestro to get the appropriate
    areas, ids and content descriptions.

--- shared/src/iosMain/kotlin/com/escodro/shared/Main.ios.kt
+++ shared/src/iosMain/kotlin/com/escodro/shared/Main.ios.kt
@@ -1,6 +1,27 @@
 package com.escodro.shared
 
+import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.ui.platform.AccessibilityDebugLogger
+import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 
+@OptIn(ExperimentalComposeApi::class)
 @Suppress("Unused", "FunctionName")
-fun MainViewController() = ComposeUIViewController { AlkaaMultiplatformApp() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        accessibilitySyncOptions = AccessibilitySyncOptions.Always(AccessibilityDebugLoggerImpl())
+    }
+) {
+    AlkaaMultiplatformApp()
+}
+
+@OptIn(ExperimentalComposeApi::class)
+private class AccessibilityDebugLoggerImpl : AccessibilityDebugLogger {
+    override fun log(message: Any?) {
+        if (message == null) {
+            println()
+        } else {
+            println(message)
+        }
+    }
+}

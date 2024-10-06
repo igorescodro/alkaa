f490e681d91d3a6838277f288fae786eb8ee8270
Igor Escodro <escodro@outlook.com>
2024-10-06 10:51:54 -0400

    🎃 Implement Destinations and Events
    New destinations and events were created to represent all the app's
    screens and how to interact between them.

---
+++ libraries/navigation/src/commonMain/kotlin/com/escodro/navigation/event/Event.kt
@@ -0,0 +1,29 @@
+package com.escodro.navigation.event
+
+import com.escodro.navigation.destination.Destination
+import com.escodro.navigation.destination.HomeDestination
+
+sealed interface Event {
+
+    fun nextDestination(): Destination
+
+    data object OnBack : Event {
+        override fun nextDestination(): Destination = Destination.Back
+    }
+
+    data object OnTaskListClick : Event {
+        override fun nextDestination(): Destination = HomeDestination.TaskList
+    }
+
+    data object OnSearchClick : Event {
+        override fun nextDestination(): Destination = HomeDestination.Search
+    }
+
+    data object OnCategoryListClick : Event {
+        override fun nextDestination(): Destination = HomeDestination.CategoryList
+    }
+
+    data object OnPreferencesClick : Event {
+        override fun nextDestination(): Destination = HomeDestination.Preferences
+    }
+}

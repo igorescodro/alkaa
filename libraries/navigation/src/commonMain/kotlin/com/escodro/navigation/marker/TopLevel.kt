916f7fa71324779f185d24a46143875c3564c304
Igor Escodro <escodro@outlook.com>
2024-10-08 21:06:15 -0400

    🃟 Implement existing destinations in the new navigation system
    All the existing destinations were migrated from Voyager to the new
    navigation system. Future commits will handle the deep links and port
    more destinations (such as confirmation dialogs) to this system.

---
+++ libraries/navigation/src/commonMain/kotlin/com/escodro/navigation/marker/TopLevel.kt
@@ -0,0 +1,11 @@
+package com.escodro.navigation.marker
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import org.jetbrains.compose.resources.StringResource
+
+interface TopLevel {
+
+    val title: StringResource
+
+    val icon: ImageVector
+}

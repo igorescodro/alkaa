57a80c0ea345baea8732c2eb35fdbaf189cca335
Igor Escodro <escodro@outlook.com>
2024-10-09 09:47:47 -0400

    🔗 Initial deep link implementation
    Since Compose Multiplatform does not fully support deep links, the
    initial implementation was created, and it will be updated when the
    support is ready. For now, every deep link will simply open Alkaa's main
    screen.

---
+++ libraries/navigation/src/androidMain/kotlin/com/escodro/navigation/deeplink/AndroidDeepLink.kt
@@ -0,0 +1,26 @@
+package com.escodro.navigation.deeplink
+
+import android.content.Intent
+import android.net.Uri
+import com.escodro.navigation.destination.Destination
+
+/**
+ * Destinations specifically for the Android platform since it requires [Intent] to navigate between
+ * screens in some flows, such as notifications and widgets.
+ */
+object AndroidDeepLink {
+
+    /**
+     * Returns the [Intent] to the home screen.
+     */
+    fun homeIntent(): Intent = Intent(Intent.ACTION_VIEW, Uri.parse(Destination.URI))
+
+    /**
+     * Returns the [Intent] to the task detail screen.
+     *
+     * @param taskId the task id to be shown
+     */
+    fun taskDetail(taskId: Long): Intent = homeIntent().apply {
+        // TODO Add the task detail when CMP supports deep links
+    }
+}

cdea424f8adf9d2cfd12250b3e1829ea7cee4d70
Igor Escodro <escodro@outlook.com>
2025-01-31 13:56:16 -0500

    🐚 Update to Compose 1.8.0-alpha03
    Let's go!

--- shared/src/iosMain/kotlin/com/escodro/shared/Main.ios.kt
+++ shared/src/iosMain/kotlin/com/escodro/shared/Main.ios.kt
@@ -1,27 +1,8 @@
 package com.escodro.shared
 
-import androidx.compose.runtime.ExperimentalComposeApi
-import androidx.compose.ui.platform.AccessibilityDebugLogger
-import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 
-@OptIn(ExperimentalComposeApi::class)
 @Suppress("Unused", "FunctionName")
-fun MainViewController() = ComposeUIViewController(
-    configure = {
-        accessibilitySyncOptions = AccessibilitySyncOptions.Always(AccessibilityDebugLoggerImpl())
-    },
-) {
+fun MainViewController() = ComposeUIViewController {
     AlkaaMultiplatformApp()
-}
-
-@OptIn(ExperimentalComposeApi::class)
-private class AccessibilityDebugLoggerImpl : AccessibilityDebugLogger {
-    override fun log(message: Any?) {
-        if (message == null) {
-            println()
-        } else {
-            println(message)
-        }
-    }
 }

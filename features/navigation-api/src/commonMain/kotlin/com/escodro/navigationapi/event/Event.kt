5e60c35df603d883798b5becb453c341adc24fa3
Igor Escodro <escodro@outlook.com>
2025-03-16 14:54:07 -0400

    🗑️ Remove unused navigation events
    Deleted redundant navigation events and the `SearchEvent` object.

--- features/navigation-api/src/commonMain/kotlin/com/escodro/navigationapi/event/Event.kt
+++ features/navigation-api/src/commonMain/kotlin/com/escodro/navigationapi/event/Event.kt
@@ -1,7 +1,6 @@
 package com.escodro.navigationapi.event
 
 import com.escodro.navigationapi.destination.Destination
-import com.escodro.navigationapi.destination.HomeDestination
 
 sealed interface Event {
 
@@ -9,21 +8,5 @@ sealed interface Event {
 
     data object OnBack : Event {
         override fun nextDestination(): Destination = Destination.Back
-    }
-
-    data object OnTaskListClick : Event {
-        override fun nextDestination(): Destination = HomeDestination.TaskList
-    }
-
-    data object OnSearchClick : Event {
-        override fun nextDestination(): Destination = HomeDestination.Search
-    }
-
-    data object OnCategoryListClick : Event {
-        override fun nextDestination(): Destination = HomeDestination.CategoryList
-    }
-
-    data object OnPreferencesClick : Event {
-        override fun nextDestination(): Destination = HomeDestination.Preferences
     }
 }

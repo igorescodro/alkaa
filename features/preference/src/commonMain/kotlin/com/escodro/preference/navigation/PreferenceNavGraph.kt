cdf7e9fa4bcf1081797cdf154476cccab12deb13
Igor Escodro <escodro@outlook.com>
2025-03-16 15:07:39 -0400

    🌇 Add adaptive navigation for Preferences
    Introduce a responsive UI for Preference navigation using Material 3
    adaptive layout. Compact views display single-pane content, while larger
    screens leverage a list-detail adaptive scaffold for enhanced usability.

--- features/preference/src/commonMain/kotlin/com/escodro/preference/navigation/PreferenceNavGraph.kt
+++ features/preference/src/commonMain/kotlin/com/escodro/preference/navigation/PreferenceNavGraph.kt
@@ -1,5 +1,6 @@
 package com.escodro.preference.navigation
 
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.escodro.designsystem.animation.SlideInHorizontallyTransition
@@ -9,6 +10,7 @@ import com.escodro.navigationapi.destination.HomeDestination
 import com.escodro.navigationapi.destination.PreferenceDestination
 import com.escodro.navigationapi.event.Event
 import com.escodro.navigationapi.event.PreferenceEvent
+import com.escodro.navigationapi.extension.isCompact
 import com.escodro.navigationapi.provider.NavGraph
 import com.escodro.preference.presentation.AboutScreen
 import com.escodro.preference.presentation.OpenSource
@@ -20,7 +22,9 @@ internal class PreferenceNavGraph(
 ) : NavGraph {
     override val navGraph: NavGraphBuilder.(NavEventController) -> Unit = { navEventController ->
         composable<HomeDestination.Preferences> {
+            val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
             PreferenceSection(
+                isCompact = windowSizeClass.isCompact(),
                 onAboutClick = { navEventController.sendEvent(PreferenceEvent.OnAboutClick) },
                 onOpenSourceClick = { navEventController.sendEvent(PreferenceEvent.OnLicensesClick) },
                 onTrackerClick = { navEventController.sendEvent(PreferenceEvent.OnTrackerClick) },

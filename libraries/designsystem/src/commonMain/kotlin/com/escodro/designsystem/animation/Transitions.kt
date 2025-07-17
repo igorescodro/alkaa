932db19ad33690113cad008a05149372f9eca0d5
Igor Escodro <escodro@outlook.com>
2025-07-17 11:50:38 -0400

    🌀 Fix new Ktlint errors
    New errors in the latest version are now fixed

--- libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/animation/Transitions.kt
+++ libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/animation/Transitions.kt
@@ -21,19 +21,21 @@ val SlideInHorizontallyTransition: EnterTransition = slideInHorizontally(
         durationMillis = 300,
         easing = LinearEasing,
     ),
-) + fadeIn()
+) +
+    fadeIn()
 
 /**
  * Transition used to slide out horizontally the content. The content will slide from the right to
  * the left and fade out.
  */
-val SlideOutHorizontallyTransition: ExitTransition = fadeOut() + slideOutHorizontally(
-    targetOffsetX = { it },
-    animationSpec = tween(
-        durationMillis = 300,
-        easing = LinearEasing,
-    ),
-)
+val SlideOutHorizontallyTransition: ExitTransition = fadeOut() +
+    slideOutHorizontally(
+        targetOffsetX = { it },
+        animationSpec = tween(
+            durationMillis = 300,
+            easing = LinearEasing,
+        ),
+    )
 
 /**
  * Transition used to slide in vertically the content. The content will slide from the top to the
@@ -44,12 +46,13 @@ val TopBarEnterTransition: EnterTransition = fadeIn(
         delayMillis = 300,
         durationMillis = 600,
     ),
-) + slideInVertically(
-    animationSpec = tween(
-        delayMillis = 300,
-        durationMillis = 600,
-    ),
-)
+) +
+    slideInVertically(
+        animationSpec = tween(
+            delayMillis = 300,
+            durationMillis = 600,
+        ),
+    )
 
 /**
  * Transition used to slide out vertically the content. The content will slide from the bottom to the
@@ -60,9 +63,10 @@ val TopBarExitTransition: ExitTransition = slideOutVertically(
         delayMillis = 0,
         durationMillis = 600,
     ),
-) + fadeOut(
-    animationSpec = tween(
-        delayMillis = 0,
-        durationMillis = 600,
-    ),
-)
+) +
+    fadeOut(
+        animationSpec = tween(
+            delayMillis = 0,
+            durationMillis = 600,
+        ),
+    )

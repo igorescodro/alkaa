bd1bc41e871c8bc187ffed6c757735b9063aad4e
Igor Escodro <escodro@outlook.com>
2025-10-28 13:41:34 -0400

    🎨 Add `CommonIgnoredOnParcel` annotation for iOS and Desktop
    Introduced the `CommonIgnoredOnParcel` annotation with `SOURCE`
    retention and `PROPERTY` target. This ensures properties marked with it
    are excluded during parceling processes in common modules.

--- libraries/parcelable/src/desktopMain/kotlin/com/escodro/parcelable/CommonParcelable.desktop.kt
+++ libraries/parcelable/src/desktopMain/kotlin/com/escodro/parcelable/CommonParcelable.desktop.kt
@@ -2,4 +2,6 @@ package com.escodro.parcelable
 
 actual interface CommonParcelable
 
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.PROPERTY)
 actual annotation class CommonIgnoredOnParcel

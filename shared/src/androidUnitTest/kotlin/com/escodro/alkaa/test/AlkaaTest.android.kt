278c8233b3f38b7b6c19dbd8d24a0ff127a90177
Igor Escodro <escodro@outlook.com>
2025-10-30 15:30:52 -0400

    🗑️ Remove unused Android-specific context references
    Eliminated `Context` and `InstrumentationRegistry` from the Android test
     module as they were no longer needed. Updated `platformModule` to
     provide an empty module, simplifying the codebase and improving
     maintainability.

--- shared/src/androidUnitTest/kotlin/com/escodro/alkaa/test/AlkaaTest.android.kt
+++ shared/src/androidUnitTest/kotlin/com/escodro/alkaa/test/AlkaaTest.android.kt
@@ -2,8 +2,6 @@
 
 package com.escodro.alkaa.test
 
-import android.content.Context
-import androidx.test.platform.app.InstrumentationRegistry
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -18,6 +16,4 @@ actual class PlatformAnimation actual constructor() {
 }
 
 actual val platformModule: Module
-    get() = module {
-        single<Context> { InstrumentationRegistry.getInstrumentation().targetContext }
-    }
+    get() = module { }

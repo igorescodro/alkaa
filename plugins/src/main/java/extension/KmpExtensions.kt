625c01dfb293e86dfeb1d32a9c05745b111acb47
Igor Escodro <escodro@outlook.com>
2025-12-30 11:52:32 -0500

    🖥️️ Improve iOS build performance
    Disabled the compiler index store during the Xcode build to improve
    performance.

--- plugins/src/main/java/extension/KmpExtensions.kt
+++ plugins/src/main/java/extension/KmpExtensions.kt
@@ -10,7 +10,6 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 fun KotlinMultiplatformExtension.setFrameworkBaseName(name: String) {
     listOf(
         iosX64(),
-        iosArm64(),
         iosSimulatorArm64(),
     ).forEach {
         it.binaries.framework {

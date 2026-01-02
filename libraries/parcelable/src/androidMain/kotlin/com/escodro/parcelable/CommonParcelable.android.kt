dd998444d0522168fa0b9756822789f3b02da954
Igor Escodro <escodro@outlook.com>
2026-01-02 12:26:21 -0500

    🔥 Fix serialization exception when Alkaa is in the foreground
    - Issue: When Alkaa goes in the foreground in the Android app, the apps
    crash due to Serialization exception in the TopLevel destinations.
    
    - Root cause: Since `rememberSaveable` uses `Serializable` under the
    hood for JVM-based variants, it will crash if the class does not
    implement it.
    
    - Solution: Update the `CommonParcelable` to also contain the custom
    `Serializable` implementation on each platform.

--- libraries/parcelable/src/androidMain/kotlin/com/escodro/parcelable/CommonParcelable.android.kt
+++ libraries/parcelable/src/androidMain/kotlin/com/escodro/parcelable/CommonParcelable.android.kt
@@ -1,5 +1,9 @@
 package com.escodro.parcelable
 
-actual typealias CommonParcelable = android.os.Parcelable
+import java.io.Serializable
+
+actual typealias CommonSerializable = Serializable
+
+actual interface CommonParcelable : android.os.Parcelable, CommonSerializable
 
 actual typealias CommonIgnoredOnParcel = kotlinx.parcelize.IgnoredOnParcel

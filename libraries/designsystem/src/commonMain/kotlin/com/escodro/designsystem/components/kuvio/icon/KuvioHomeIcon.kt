667d20e05f75313d1ba21ee1127cf4f31e4445d0
Igor Escodro <escodro@outlook.com>
2026-03-07 11:39:25 -0500

    🪞 Use AutoMirrored Comment icon
    Updated `KuvioCommentIcon` to use `Icons.AutoMirrored.Filled.Comment`
    instead of the default icon. Also, removed unused import in
    `KuvioHomeIcon`.

--- libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/components/kuvio/icon/KuvioHomeIcon.kt
+++ libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/components/kuvio/icon/KuvioHomeIcon.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview

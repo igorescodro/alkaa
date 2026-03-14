6327596ee4ef68259eb6173baa7d1336d41ebffe
Igor Escodro <escodro@outlook.com>
2026-03-14 18:03:30 -0400

    🧹 Reorganize Kuvio button components and clean up unused icons
    Moved `KuvioAddButton` to the `icon.button` package to group it with
    similar components, and updated the corresponding import reference in
    `KuvioAddTaskBar`.
    
    Deleted `KuvioListIcon` and `KuvioNotificationsIcon` from the design
    system as they are no longer needed.

--- libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/components/kuvio/bar/KuvioAddTaskBar.kt
+++ libraries/designsystem/src/commonMain/kotlin/com/escodro/designsystem/components/kuvio/bar/KuvioAddTaskBar.kt
@@ -30,7 +30,7 @@ import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.escodro.designsystem.components.kuvio.button.KuvioAddButton
+import com.escodro.designsystem.components.kuvio.icon.button.KuvioAddButton
 import com.escodro.designsystem.components.kuvio.icon.button.KuvioCalendarIcon
 import com.escodro.designsystem.components.kuvio.text.KuvioBodyMediumText
 import com.escodro.designsystem.theme.AlkaaThemePreview
